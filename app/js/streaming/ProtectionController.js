18a2eb37395b8e81c57dfe7dd54c69689f18e839
Daniel Hedenius <daniel.hedenius@gmail.com>
2014-09-29 15:03:39 +0200

    Bugfix for passing laURL to getUpdate

--- app/js/streaming/ProtectionController.js
+++ app/js/streaming/ProtectionController.js
@@ -73,10 +73,10 @@ MediaPlayer.dependencies.ProtectionController = function () {
             }
         },
 
-        updateFromMessage = function (kid, session, sessionId, msg, laURL) {
+        updateFromMessage = function (kid, session, sessionId, rawMessage, uint16Message, laURL) {
             var self = this,
                 result;
-            result = self.protectionModel.updateFromMessage(kid, sessionId, msg, laURL, element);
+            result = self.protectionModel.updateFromMessage(kid, sessionId, rawMessage, uint16Message, laURL);
             result.then(
                 function (data) {
                     session.update(data);
